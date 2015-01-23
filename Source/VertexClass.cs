0b7cc5c84d121618a516ad6f6f5db781f0e77130
mpreble <saihtam24@gmail.com>
2015-01-23 03:06:30 -0600

    adding vertexClass

---
+++ Source/VertexClass.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class VertexClass {
+	
+	public Vector3 pos;
+	public Vector3 norm;
+	public Vector2 uv;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
